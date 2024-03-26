a443cfa8acaa8b933d2ff9c31d22256c5ec70e6c
Chay Hyunmin Park <123585452+Chay-HyunminPark@users.noreply.github.com>
2024-03-26 18:35:10 -0400

    Update 00-simulate_data.R

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -6,8 +6,7 @@
 # Date: 19 March 2024
 # Contact: chay.park@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: install package, tidyverse
 
 
 #### Workspace setup ####
